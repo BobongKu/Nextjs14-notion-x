1516cf3988c5f287f4657c5dc5598fbaad1d82d6
DevBO <42234678+BobongKu@users.noreply.github.com>
2024-03-09 20:45:46 +0900

    insights patch

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
             <footer className="body-font">
                 <div>
                     <div className="flex px-5 py-6">
-                    <a className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4 px-2" href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fbobong.blog&count_bg=%23A5AAA0&title_bg=%23555555&icon=protocols-dot-io.svg&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
-                    <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4 px-2">© 2024 Bobong</p>
+                    <a className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4 px-2" href="https://hits.seeyoufarm.com"><img alt="hits" src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fbobong.blog&count_bg=%23A5AAA0&title_bg=%23555555&icon=protocols-dot-io.svg&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
+                    <p className="text-sm text-gray-400 sm:ml-6 sm:mt-0 mt-4 px-2">© 2024 Bobong</p>
                     </div>
                 </div>
             </footer>
